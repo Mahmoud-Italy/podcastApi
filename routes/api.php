797045d3d9e9ce25147a89d7602c9fcb579d01d2
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2020-12-08 17:08:22 +0100

    handel new routes

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('s1', S1Controller::class);
-Route::apiResource('s2', S2Controller::class);
-Route::apiResource('s3', S3Controller::class);
+// Route::apiResource('s1', S1Controller::class);
+// Route::apiResource('s2', S2Controller::class);
+// Route::apiResource('s3', S3Controller::class);
+
+
+Route::get('/s1', function () {
+    return response()->json('{"comment":["Produktion: Audiowiese&#13;&#10;Text: Jens Wenzel&#13;&#10;Sprecher: Juliane Zschau, Jens Wenzel&#13;&#10;Im Auftrag von podcast.de"],"year":["2009"],"artist":["www.podcast.de","Jens Wenzel"],"title":["Wie funktioniert ein Podcast?"]}');
+});
+
+Route::get('/s2', function () {
+    return response()->json('{"language":["Undetermined"],"encoding_tool":["Lavf57.25.100"]}');
+});
+
+Route::get('/s3', function () {
+    return response()->json('');
+});
